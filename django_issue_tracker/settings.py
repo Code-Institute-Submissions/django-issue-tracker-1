f5e769c9ef38bd4e2d81abe0a0965721aefa3f4a
Marcus Hornby <marcushornby@outlook.com>
2018-12-10 18:26:12 +0000

    remove default secret key

--- django_issue_tracker/settings.py
+++ django_issue_tracker/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rh3nf%=+uz@ze_t)nmuyi-+#kwh(555kxuvz)l!=4*&j%&i#7g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
