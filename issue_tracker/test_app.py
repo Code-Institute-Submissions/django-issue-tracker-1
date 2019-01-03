f24f9048e11f2ec8930a8eec62161f5364b0b690
Marcus Hornby <marcushornby@outlook.com>
2019-01-03 23:04:20 +0000

    removed test coverage

--- issue_tracker/test_app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import apps
-from django.test import TestCase
-from .apps import IssueTrackerConfig
-
-class TestIssueTrackerConfig(TestCase):
-    def test_app(self):
-        self.assertEqual("issue_tracker", IssueTrackerConfig.name)
-        self.assertEqual("issue_tracker", apps.get_app_config("issue_tracker").name)
