22083b306ce2f8bcbad1a7968d9e27c941137f2c
Marcus Hornby <marcushornby@outlook.com>
2019-01-03 07:58:00 +0000

    removing api keys from github

--- env.py
+++ env.py
@@ -2,3 +2,7 @@ import os
 
 os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_test_fWHv8c38KcsbAkyv64MjdvHi")
 os.environ.setdefault("STRIPE_SECRET", "sk_test_CG5DPiogqRDP5CW9pqjyXcMm")
+os.environ.setdefault("DATABASE_URL", "postgres://rcjnahzkbkzoni:cecc1330a921665caf6185411af737d4121fbd0f5d2183a8fe8f72fa9be7a02d@ec2-54-246-85-234.eu-west-1.compute.amazonaws.com:5432/d7cqdqafik4k3n")
+os.environ.setdefault("SECRET_KEY", ")5hpv_bs#(cbj*0vd(po)^wzcvqzgi0voa06mdkgcxo@%")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAJKXYTQAGDBFXYIZQ")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "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")
