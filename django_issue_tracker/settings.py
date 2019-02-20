0eea3a107a9006397f94f289a9b7d46a99618a2d
Marcus Hornby <marcushornby@outlook.com>
2019-02-20 17:28:07 +0000

    added whitenoise to settings.py file

--- django_issue_tracker/settings.py
+++ django_issue_tracker/settings.py
@@ -164,6 +164,7 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
