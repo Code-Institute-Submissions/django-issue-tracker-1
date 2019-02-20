aefa52978b96462173538f595510ff164613e30c
Marcus Hornby <marcushornby@outlook.com>
2019-02-20 17:20:23 +0000

    disabled import env

--- posts/views.py
+++ posts/views.py
@@ -12,7 +12,7 @@ def get_posts(request):
     to a blogposts.html template
     """
     
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-views')
     return render(request, "blogposts.html", {'posts': posts})
     
 def post_detail(request, pk):
