545154dd4b3792336339cda9c9d27ca7f00c7ffd
Marcus Hornby <marcushornby@outlook.com>
2019-02-20 17:42:54 +0000

    order by added to products, images moved to correct folder

--- products/views.py
+++ products/views.py
@@ -4,5 +4,5 @@ from .models import Product
 # Create your views here.
 
 def all_products(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('price')
     return render(request, 'products.html', {"products": products})
